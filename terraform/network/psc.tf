1a275c7f22991570f1fe780c37c0733b1351ddcc
TMahalingam <tharun.mahalingam@colt.net>
2026-04-14 00:41:10 +0530

    PSC and Cloud NAT

---
+++ terraform/network/psc.tf
@@ -0,0 +1,18 @@
+resource "google_compute_global_address" "aicoe_psc_address" {
+  name          = "${var.project}${var.region}-psc-google-apis-ip"
+  address_type  = "INTERNAL"
+  purpose       = "PRIVATE_SERVICE_CONNECT"
+  network       = google_compute_network.aicoe_network.id
+  address       = "192.168.1.3"
+}
+
+# IMPORTANT: PSC forwarding rule names for Google API bundles (all-apis, vpc-sc)
+# must be 1-20 characters, lowercase letters and numbers only, starting with a letter.
+# Hyphens are NOT allowed.
+resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
+  name                  = "${var.project}${var.region}-pscapis"
+  network               = google_compute_network.aicoe_network.id
+  ip_address            = google_compute_global_address.aicoe_psc_address.id
+  target                = "all-apis"
+  load_balancing_scheme = ""
+}
