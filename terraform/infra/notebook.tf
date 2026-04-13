2fdb702d9c30a52b996b6846698be42116678a72
TMahalingam <tharun.mahalingam@colt.net>
2026-04-14 03:27:16 +0530

    Vertex AI Notebook

--- terraform/infra/notebook.tf
+++ terraform/infra/notebook.tf
@@ -1,7 +1,7 @@
-resource "google_workbench_instance" "vertex_ai_workbench" {
+resource "google_workbench_instance" "aicoe-vertex_ai_workbench" {
 
   name        = "${var.project}${var.envname}-notebook"
-  location    = var.region
+  location    = "${var.region}-b"
   project     = "${var.project}${var.envname}"
   instance_id = "${var.project}${var.envname}-notebook"
   
@@ -32,7 +32,11 @@ resource "google_workbench_instance" "vertex_ai_workbench" {
           disk_type    = var.data_disk_type    # e.g. "PD_SSD"
         }
       }
-
+    
+     labels = {
+        env    = var.envname
+        system = "${var.project}${var.envname}"
+      }
       
   }
 
