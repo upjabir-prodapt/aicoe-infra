5a8288bad681494ab51d1774e769c4fe4b761732
TMahalingam <tharun.mahalingam@colt.net>
2026-04-16 18:40:05 +0530

    bq-dataset

---
+++ terraform/infra/bq.tf
@@ -0,0 +1,13 @@
+resource "google_bigquery_dataset" "aicoe_translation_dataset" {
+  dataset_id               = "${var.project}${var.envname}-translation-dataset"
+  location                 = var.region
+  project                  = "${var.project}${var.envname}"
+
+  # Optional safety flag: defaults to false if not set
+  delete_contents_on_destroy = true
+
+  labels = {
+      env    = var.envname
+      system = "${var.project}${var.envname}"
+    }
+}
