8c0408629b5363405b68fb7ae575949d061acb6b
TMahalingam <tharun.mahalingam@colt.net>
2026-04-16 21:08:18 +0530

    sales-bq-dataset

--- terraform/infra/bq.tf
+++ terraform/infra/bq.tf
@@ -11,3 +11,20 @@ resource "google_bigquery_dataset" "aicoe_translation_dataset" {
       system = "${var.project}${var.envname}"
     }
 }
+
+###########################Sales Agent Dataset#############################
+
+resource "google_bigquery_dataset" "aicoe_sales_agent_dataset" {
+  dataset_id               = "${var.project}${var.envname}_sales_agent_dataset"
+  location                 = var.region
+  project                  = "${var.project}${var.envname}"
+
+  # Optional safety flag: defaults to false if not set
+  delete_contents_on_destroy = true
+
+  labels = {
+      env    = var.envname
+      system = "${var.project}${var.envname}"
+    }
+}
+
