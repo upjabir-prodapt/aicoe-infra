d442c3fe9822de89189867a1c024ba45b572c7b1
BL <LBL@INTERNAL.COLT.NET>
2026-04-24 18:21:56 +0530

    feat: adding ILB for translation-api-service

--- terraform/infra/load_balancer.tf
+++ terraform/infra/load_balancer.tf
@@ -1,19 +1,29 @@
 # ILB for primary region
-resource "google_compute_region_backend_service" "aicoe_ilb_be" {
-  name   = "${var.project}${var.envname}-ilb-be"
+resource "google_compute_region_backend_service" "aicoe_ilb_translation_be" {
+  name   = "${var.project}${var.envname}-ilb-translation-be"
   region = var.region
   project = "${var.project}${var.envname}" 
   load_balancing_scheme = "INTERNAL_MANAGED"
   protocol = "HTTPS"
 
   backend {
-    group = google_compute_region_network_endpoint_group.aicoe_serverless_neg.id
+    group = google_compute_region_network_endpoint_group.aicoe_serverless_neg_translation.id
     balancing_mode = "UTILIZATION"
   }
 }
  
-resource "google_compute_region_network_endpoint_group" "aicoe_serverless_neg" {
-  name                  = "${var.project}${var.envname}-serverless-neg"
+# resource "google_compute_region_network_endpoint_group" "aicoe_serverless_neg" {
+#  name                  = "${var.project}${var.envname}-serverless-neg"
+#  network_endpoint_type = "SERVERLESS"
+#  region                = var.region
+
+#  cloud_run {
+#    service = var.cloud_run_service_name
+#  }
+# }
+
+resource "google_compute_region_network_endpoint_group" "aicoe_serverless_neg_translation" {
+  name                  = "${var.project}${var.envname}-serverless-neg-translation"
   network_endpoint_type = "SERVERLESS"
   region                = var.region
 
@@ -23,10 +33,18 @@ resource "google_compute_region_network_endpoint_group" "aicoe_serverless_neg" {
 }
 
 resource "google_compute_region_url_map" "aicoe_ilb_url_map" {
-    name    = "${var.project}${var.envname}-ilb-url-map"
-    project = "${var.project}${var.envname}"
-    region  = var.region
-    default_service = google_compute_region_backend_service.aicoe_ilb_be.id
+  name    = "${var.project}${var.envname}-ilb-url-map"
+  project = "${var.project}${var.envname}"
+  region  = var.region
+  default_service = google_compute_region_backend_service.aicoe_ilb_translation_be.id
+}
+
+resource "google_compute_region_target_https_proxy" "aicoe_ilb_https_proxy" {
+  name    = "${var.project}${var.envname}-ilb-https-proxy"
+  project = "${var.project}${var.envname}"
+  region  = var.region
+  url_map = google_compute_region_url_map.aicoe_ilb_url_map.id
+  ssl_certificates = [google_compute_region_ssl_certificate.aicoe_translation_ssl.id]
 }
  
 resource "google_compute_forwarding_rule" "aicoe_ilb_forwarding_rule" {
@@ -35,7 +53,7 @@ resource "google_compute_forwarding_rule" "aicoe_ilb_forwarding_rule" {
   region                = var.region
   network               = data.terraform_remote_state.network.outputs.aicoe_network
   subnetwork            = data.terraform_remote_state.network.outputs.aicoe_subnet_name
-  target                = google_compute_region_url_map.aicoe_ilb_url_map.id
+  target                = google_compute_region_target_https_proxy.aicoe_ilb_https_proxy.id
   port_range            = 443
   ip_address            = data.terraform_remote_state.network.outputs.aicoe_staticip_ilb
   load_balancing_scheme = "INTERNAL_MANAGED"
@@ -45,4 +63,23 @@ resource "google_compute_forwarding_rule" "aicoe_ilb_forwarding_rule" {
       system = "${var.project}${var.envname}"
     }
   
+}
+
+resource "google_compute_region_ssl_certificate" "aicoe_translation_ssl" {
+  name                  = "${var.project}${var.envname}-translation-ssl"
+  project               = "${var.project}${var.envname}"
+  region                = var.region
+
+  certificate           = data.google_storage_bucket_object_content.ssl_certificate.google_storage_bucket_object_content
+  private_key           = data.google_storage_bucket_object_content.ssl_private_key.google_storage_bucket_object_content
+  }
+
+data "google_storage_bucket_object_content" "ssl_certificate" {
+  bucket = var.ssl_bucket_name
+  name   = var.ssl_certificate
+}
+
+data "google_storage_bucket_object_content" "ssl_private_key" {
+  bucket = var.ssl_bucket_name
+  name   = var.ssl_private_key
 }
