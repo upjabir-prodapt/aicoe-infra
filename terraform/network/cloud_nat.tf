1a4d0ec6598d282ad149fa2fc963559fcc8a095a
BL <LBL@INTERNAL.COLT.NET>
2026-04-27 17:49:07 +0530

    feat: dev platform setup

--- terraform/network/cloud_nat.tf
+++ terraform/network/cloud_nat.tf
@@ -6,7 +6,7 @@ resource "google_compute_router" "aicoe_router_cloudnat" {
   count   = var.envname == "sandox" ? 1 : 0
   project = "${var.project}${var.envname}"
   name    = "${var.project}${var.envname}-router-cloudnat"
-  network = google_compute_network.aicoe_network.self_link
+  network = google_compute_network.aicoe_network[0].self_link
   region  = var.region
 }
  
@@ -17,12 +17,12 @@ resource "google_compute_router" "aicoe_router_cloudnat" {
 resource "google_compute_router_nat" "aicoe_cloudnat" {
   count                  = var.envname == "sandox" ? 1 : 0
   name                   = "${var.project}${var.envname}-cloudnat"
-  router                 = google_compute_router.aicoe_router_cloudnat.name
+  router                 = google_compute_router.aicoe_router_cloudnat[0].name
   region                 = var.region
   nat_ip_allocate_option = "MANUAL_ONLY"
   nat_ips = [
-    google_compute_address.aicoe_staticip_nat_1.self_link,
-    google_compute_address.aicoe_staticip_nat_2.self_link
+    google_compute_address.aicoe_staticip_nat_1[0].self_link,
+    google_compute_address.aicoe_staticip_nat_2[0].self_link
   ]
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
  
