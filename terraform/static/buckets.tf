6e48db1ab14a682a2a33deaf2437a0c94bd506d9
BL <LBL@INTERNAL.COLT.NET>
2026-04-29 13:49:11 +0530

    feat: buckets for usecases

--- terraform/static/buckets.tf
+++ terraform/static/buckets.tf
@@ -39,3 +39,45 @@ resource "google_storage_bucket" "aicoe_vxai_bs_bucket" {
     system = "${var.project}${var.envname}"
   }
 }
+
+###########################################
+## AICOE Translation Application Bucket  ##
+###########################################
+ 
+resource "google_storage_bucket" "aicoe_trans_app_bucket" {
+  name          = "${var.project}${var.envname}-vxai-translation-app-001"
+  location      = var.region
+  project       = "${var.project}${var.envname}"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+ 
+  versioning {
+    enabled = true
+  }
+  
+  labels = {
+    env    = var.envname
+    system = "${var.project}${var.envname}"
+  }
+}
+
+###########################################
+###   AICOE Sales Application Bucket    ###
+###########################################
+ 
+resource "google_storage_bucket" "aicoe_sales_app_bucket" {
+  name          = "${var.project}${var.envname}-vxai-sales-app-001"
+  location      = var.region
+  project       = "${var.project}${var.envname}"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+ 
+  versioning {
+    enabled = true
+  }
+  
+  labels = {
+    env    = var.envname
+    system = "${var.project}${var.envname}"
+  }
+}
