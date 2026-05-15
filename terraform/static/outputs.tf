501f6621af6f3109fa3cd1180f9f16f7c9d8e38f
BL <LBL@INTERNAL.COLT.NET>
2026-05-15 20:31:53 +0530

    prod setup

--- terraform/static/outputs.tf
+++ terraform/static/outputs.tf
@@ -2,5 +2,5 @@ output "aicoe_app_bucket_key_id" {
   value = google_kms_crypto_key.aicoe_app_bucket_key.id
 }
 output "aicoe_vxai_wkb_key_id" {
-  value = google_kms_crypto_key.aicoe_vxai_wkb_key.id
+  value = try(google_kms_crypto_key.aicoe_vxai_wkb_key[0].id, null)
 }
