76f58356f532d742d2d8ca7d88db83a25d4dbbf2
BL <LBL@INTERNAL.COLT.NET>
2026-05-18 13:54:36 +0530

    prodsetup

--- terraform/static/buckets.tf
+++ terraform/static/buckets.tf
@@ -52,48 +52,48 @@
 # ## AICOE Translation Application Bucket  ##
 # ###########################################
  
-# resource "google_storage_bucket" "aicoe_trans_app_bucket" {
-#   name          = "${var.project}${var.envname}-vxai-translation-app-001"
-#   location      = var.region
-#   project       = "${var.project}${var.envname}"
-#   storage_class = "STANDARD"
-#   uniform_bucket_level_access = true
+resource "google_storage_bucket" "aicoe_trans_app_bucket" {
+  name          = "${var.project}${var.envname}-vxai-translation-app-001"
+  location      = var.region
+  project       = "${var.project}${var.envname}"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
  
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   # encryption {
-#   #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
-#   #       }
+  # encryption {
+  #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+  #       }
   
-#   labels = {
-#     env    = var.envname
-#     system = "${var.project}${var.envname}"
-#   }
-# }
+  labels = {
+    env    = var.envname
+    system = "${var.project}${var.envname}"
+  }
+}
 
-# ###########################################
-# ###   AICOE Sales Application Bucket    ###
-# ###########################################
+###########################################
+###   AICOE Sales Application Bucket    ###
+###########################################
  
-# resource "google_storage_bucket" "aicoe_sales_app_bucket" {
-#   name          = "${var.project}${var.envname}-vxai-sales-app-001"
-#   location      = var.region
-#   project       = "${var.project}${var.envname}"
-#   storage_class = "STANDARD"
-#   uniform_bucket_level_access = true
+resource "google_storage_bucket" "aicoe_sales_app_bucket" {
+  name          = "${var.project}${var.envname}-vxai-sales-app-001"
+  location      = var.region
+  project       = "${var.project}${var.envname}"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
  
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   # encryption {
-#   #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
-#   #       }
+  # encryption {
+  #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+  #       }
   
-#   labels = {
-#     env    = var.envname
-#     system = "${var.project}${var.envname}"
-#   }
-# }
+  labels = {
+    env    = var.envname
+    system = "${var.project}${var.envname}"
+  }
+}
