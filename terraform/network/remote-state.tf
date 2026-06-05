3162d3ef31c899ed13ac6ce87eaea7004a159f7c
BL <LBL@INTERNAL.COLT.NET>
2026-06-05 20:38:43 +0530

    firewalls for dev

--- terraform/network/remote-state.tf
+++ terraform/network/remote-state.tf
@@ -9,3 +9,15 @@ data "terraform_remote_state" "static" {
     prefix = "tfstate-static"
   }
 }
+
+###########################################
+### Infra Layer Remote TF State file   ###
+###########################################
+
+data "terraform_remote_state" "infra" {
+  backend = "gcs"
+  config = {
+    bucket = "${var.project}${var.envname}-bucket-tf-state"
+    prefix = "tfstate-infra"
+  }
+}
