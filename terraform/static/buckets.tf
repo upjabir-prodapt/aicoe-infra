a79ca92224b2774338cebaa19bbc05852aa3e139
BL <LBL@INTERNAL.COLT.NET>
2026-06-05 22:09:28 +0530

    Vector index for prod

--- terraform/static/buckets.tf
+++ terraform/static/buckets.tf
@@ -1,27 +1,27 @@
-# ###########################################
-# ###   AICOE Vertex Application Bucket   ###
-# ###########################################
+###########################################
+###   AICOE Vertex Application Bucket   ###
+###########################################
  
-# resource "google_storage_bucket" "aicoe_app_bucket" {
-#   name          = "${var.project}${var.envname}-vx-app-001"
-#   location      = var.region
-#   project       = "${var.project}${var.envname}"
-#   storage_class = "STANDARD"
-#   uniform_bucket_level_access = true
+resource "google_storage_bucket" "aicoe_app_bucket" {
+  name          = "${var.project}${var.envname}-vx-app-001"
+  location      = var.region
+  project       = "${var.project}${var.envname}"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
  
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
   
-#   # encryption {
-#   #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
-#   #       }
+  # encryption {
+  #          default_kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+  #       }
 
-#   labels = {
-#     env    = var.envname
-#     system = "${var.project}${var.envname}"
-#   }
-# }
+  labels = {
+    env    = var.envname
+    system = "${var.project}${var.envname}"
+  }
+}
 
 # ###########################################
 # ###   AICOE Vertex AI Bootstrap Bucket   ###
