81fd11a1f128fd8fd31d087d6c763928ff374401
BL <LBL@INTERNAL.COLT.NET>
2026-06-10 13:12:08 +0530

    bq tables

--- terraform/network/psc.tf
+++ terraform/network/psc.tf
@@ -1,21 +1,21 @@
-resource "google_compute_global_address" "aicoe_psc_address" {
-  name          = "${var.project}${var.envname}-psc-google-apis-ip"
-  address_type  = "INTERNAL"
-  purpose       = "PRIVATE_SERVICE_CONNECT"
-  network       = google_compute_network.aicoe_network.id
-  address       = "10.110.73.4"
-}
+# resource "google_compute_global_address" "aicoe_psc_address" {
+#   name          = "${var.project}${var.envname}-psc-google-apis-ip"
+#   address_type  = "INTERNAL"
+#   purpose       = "PRIVATE_SERVICE_CONNECT"
+#   network       = google_compute_network.aicoe_network.id
+#   address       = "10.110.73.4"
+# }
 
-# IMPORTANT: PSC forwarding rule names for Google API bundles (all-apis, vpc-sc)
-# must be 1-20 characters, lowercase letters and numbers only, starting with a letter.
-# Hyphens are NOT allowed.
-resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
-  name                  = "${var.project}${var.envname}pscapis"
-  network               = google_compute_network.aicoe_network.id
-  ip_address            = google_compute_global_address.aicoe_psc_address.id
-  target                = "all-apis"
-  load_balancing_scheme = ""
-}
+# # IMPORTANT: PSC forwarding rule names for Google API bundles (all-apis, vpc-sc)
+# # must be 1-20 characters, lowercase letters and numbers only, starting with a letter.
+# # Hyphens are NOT allowed.
+# resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
+#   name                  = "${var.project}${var.envname}pscapis"
+#   network               = google_compute_network.aicoe_network.id
+#   ip_address            = google_compute_global_address.aicoe_psc_address.id
+#   target                = "all-apis"
+#   load_balancing_scheme = ""
+# }
 
 
 # -----------------------------------------------------------------------------
