3b4ec76b71c9aa34f0a0c9d74facad29ed4ffd49
BL <LBL@INTERNAL.COLT.NET>
2026-06-10 13:19:51 +0530

    PSC IP update

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -27,9 +27,9 @@ output "aicoe_subnet_cidr" {
   value = var.aicoe_subnet_cidr_range
 }
 
-output "psc_endpoint_ip" {
-  value       = google_compute_global_address.aicoe_psc_address.address
-}
+# output "psc_endpoint_ip" {
+#   value       = google_compute_global_address.aicoe_psc_address.address
+# }
 
 output "dns_zone_name" {
   value       = google_dns_managed_zone.aicoe_googleapis_private.name
