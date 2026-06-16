ed3b393738f8c2092fa36437b022c6bd072f00a8
BL <LBL@INTERNAL.COLT.NET>
2026-06-16 13:33:12 +0530

    adding firestore

--- terraform/static/outputs.tf
+++ terraform/static/outputs.tf
@@ -6,3 +6,9 @@ output "aicoe_app_sa_email" {
   value       = google_service_account.aicoe_app_sa.email
   description = "Application service account used by Cloud Run and vector search APIs"
 }
+
+output "aicoe_vertex_sa_email" {
+  value       = google_service_account.aicoe_vertex_sa.email
+  description = "Service account used by Vertex AI"
+}
+
