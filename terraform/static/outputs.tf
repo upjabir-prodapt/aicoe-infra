db1268d9f2bc62963d3758e3d70b522fdc165740
BL <LBL@INTERNAL.COLT.NET>
2026-06-16 11:55:43 +0530

    updating state file

--- terraform/static/outputs.tf
+++ terraform/static/outputs.tf
@@ -1,8 +1,3 @@
 output "aicoe_app_bucket_key_id" {
   value = google_kms_crypto_key.aicoe_app_bucket_key.id
 }
-
-output "aicoe_app_sa_email" {
-  value       = google_service_account.aicoe_app_sa.email
-  description = "Application service account used by Cloud Run and vector search APIs"
-}
