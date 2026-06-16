9a3c55c924d1eea07b8ee3d5262ed5f4f41a4989
BL <LBL@INTERNAL.COLT.NET>
2026-06-16 17:17:51 +0530

    adding aihub secrets

--- terraform/static/secrets.tf
+++ terraform/static/secrets.tf
@@ -170,3 +170,65 @@ resource "google_secret_manager_secret" "aicoedev_salesagent_csr" {
      managed_by = "terraform"
  }
 }
+
+resource "google_secret_manager_secret" "aicoedev_aihub_ssl_private_key" {
+  secret_id = "aicoedev-aihub-ssl-private-key"
+  project = "${var.project}${var.envname}"
+
+  replication {
+    user_managed {
+       replicas {
+            location = var.region
+            customer_managed_encryption {
+            kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+            }
+       }
+    }
+  }
+   labels = {
+     environment = var.envname
+     managed_by = "terraform"
+ }
+}
+
+ 
+resource "google_secret_manager_secret" "aicoedev_aihub_ssl_certificate" {
+  secret_id = "aicoedev-aihub-ssl-certificate"
+  project = "${var.project}${var.envname}"
+
+  replication {
+    user_managed {
+       replicas {
+            location = var.region
+            customer_managed_encryption {
+            kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+            }
+       }
+    }
+  }
+   labels = {
+     environment = var.envname
+     managed_by = "terraform"
+ }
+}
+
+ 
+resource "google_secret_manager_secret" "aicoedev_aihub_csr" {
+  secret_id = "aicoedev-aihub-csr"
+  project = "${var.project}${var.envname}"
+
+  replication {
+    user_managed {
+       replicas {
+            location = var.region
+            customer_managed_encryption {
+            kms_key_name = google_kms_crypto_key.aicoe_app_bucket_key.id
+            }
+       }
+    }
+  }
+   labels = {
+     environment = var.envname
+     managed_by = "terraform"
+ }
+}
