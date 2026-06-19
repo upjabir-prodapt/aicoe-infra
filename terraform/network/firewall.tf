cbafa3e40d403c2eb64496330924244e06deb39f
BL <LBL@INTERNAL.COLT.NET>
2026-06-19 21:49:53 +0530

    infra build

--- terraform/network/firewall.tf
+++ terraform/network/firewall.tf
@@ -32,7 +32,7 @@ resource "google_compute_firewall" "aicoe_egress_allow_google_apis_psc" {
   description        = "Allow egress to Google APIs Private Service Connect endpoint"
   direction          = "EGRESS"
   priority           = 65534
-  destination_ranges = ["192.168.2.3", "10.110.73.5/32"]
+  destination_ranges = ["192.168.2.3"]
 
   allow {
     protocol = "tcp"
--- terraform/network/psc.tf
+++ terraform/network/psc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_global_address" "aicoe_psc_address" {
-  name          = "${var.project}-psc-google-apis-ip"
+  name          = "vegadev-psc-google-apis-ip"
   address_type  = "INTERNAL"
   purpose       = "PRIVATE_SERVICE_CONNECT"
   network       = google_compute_network.aicoe_network.id
@@ -10,7 +10,7 @@ resource "google_compute_global_address" "aicoe_psc_address" {
 # must be 1-20 characters, lowercase letters and numbers only, starting with a letter.
 # Hyphens are NOT allowed.
 resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
-  name                  = "${var.project}pscapis"
+  name                  = "vegadevpscapis"
   network               = google_compute_network.aicoe_network.id
   ip_address            = google_compute_global_address.aicoe_psc_address.id
   target                = "all-apis"
