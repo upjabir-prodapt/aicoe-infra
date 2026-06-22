b606abae337c2f7e2d91ae25d1e45e6673724a89
BL <LBL@INTERNAL.COLT.NET>
2026-06-22 15:53:53 +0530

    UI sa

--- terraform/static/sa.tf
+++ terraform/static/sa.tf
@@ -21,4 +21,19 @@ resource "google_project_iam_member" "aicoe_app_sa_iam" {
   role    = each.value
   member  = "serviceAccount:${google_service_account.aicoe_app_sa.email}"
 }
+
+#Service account for UI service
+resource "google_service_account" "aicoe_ui_sa" {
+  account_id   = "${var.project}${var.envname}-ui-sa"
+  display_name = "Service Account for UI"
+}
+
+resource "google_project_iam_member" "aicoe_ui_sa_iam" {
+  for_each = toset([
+    "roles/run.invoker",
+  ])
+  project = "${var.project}${var.envname}"
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.aicoe_app_sa.email}"
+}
  
