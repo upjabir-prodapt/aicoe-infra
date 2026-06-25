f3621dbe004475cff30ca0bbbf84e3340638c389
BL <LBL@INTERNAL.COLT.NET>
2026-06-25 22:05:00 +0530

    vegadev psc ip

--- terraform/network/psc.tf
+++ terraform/network/psc.tf
@@ -3,18 +3,18 @@ resource "google_compute_global_address" "aicoe_psc_address" {
   address_type  = "INTERNAL"
   purpose       = "PRIVATE_SERVICE_CONNECT"
   network       = google_compute_network.aicoe_network.id
-  address       = "10.6.4.65"
+  address       = "10.6.4.66"
 }
 
 # IMPORTANT: PSC forwarding rule names for Google API bundles (all-apis, vpc-sc)
 # must be 1-20 characters, lowercase letters and numbers only, starting with a letter.
 # Hyphens are NOT allowed.
-# resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
-#   name                  = "vegadevpscapis"
-#   network               = google_compute_network.aicoe_network.id
-#   ip_address            = google_compute_global_address.aicoe_psc_address.id
-#   target                = "all-apis"
-#   load_balancing_scheme = ""
-# }
+resource "google_compute_global_forwarding_rule" "aicoe_psc_google_apis" {
+  name                  = "vegadevpscapis"
+  network               = google_compute_network.aicoe_network.id
+  ip_address            = google_compute_global_address.aicoe_psc_address.id
+  target                = "all-apis"
+  load_balancing_scheme = ""
+}
 
 
