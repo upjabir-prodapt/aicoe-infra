a8c4ca0abbb791c94f0afd21abe6acccb9e52d3f
Jyoti R <jyoti.r@prodapt.com>
2026-06-26 19:53:44 +0530

    deleted azure pipeline

--- terraform/static/iam.tf
+++
@@ -1,9 +0,0 @@
-# resource "google_project_iam_member" "aicoe_group" {
-#     for_each = toset([
-#         "roles/viewer",
-#         "roles/aiplatform.user",
-#     ])
-#     project = "${var.project}"
-#     role = each.value
-#     member = "group:gcp-aicoeaiworkshop-users@colt.net"
-# }
---
+++ terraform/static/params/dev/params.tfvars
@@ -0,0 +1,9 @@
+project = "ai-product-security"
+envname = "dev"
+region = "europe-west1"
+gcp_apis_required =[
+
+    "aiplatform.googleapis.com"              # Vertex AI API - models, endpoints, pipelines, training
+      
+   
+]
