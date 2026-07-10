d18989e9b2950c4a5b660a11d32ac7ced74dcd02
riyazcloudengineer <riyazcloudengineer@gmail.com>
2026-07-10 17:23:31 +0530

    updated the terraform output.tf in infra

--- terraform/projects/aicoedev/infra/outputs.tf
+++ terraform/projects/aicoedev/infra/outputs.tf
@@ -2,62 +2,19 @@
 ###       Infra                         ###
 ###########################################
 
-###output "vector_search_service_attachment" {
-###  value = module.vector_search.vector_search_service_attachment_uri
-###}
-###
-###output "vector_search_index_id" {
-###  value = module.vector_search.vector_search_index_id
-###}
-###
-###output "vector_search_index_endpoint_id" {
-###  value = module.vector_search.vector_search_index_endpoint_id
-###}
-###
-###output "vector_search_deployed_index_id" {
-###  value = module.vector_search.vector_search_deployed_index_id
-###}
-
-output "aicoe_network" {
-  value = module.network_base.network_self_link
+output "vector_search_service_attachment" {
+  value = module.vector_search.vector_search_service_attachment_uri
 }
 
-output "aicoe_network_id" {
-  value = module.network_base.network_id
+output "vector_search_index_id" {
+  value = module.vector_search.vector_search_index_id
 }
 
-output "aicoe_subnet_name" {
-  value = module.network_base.subnet_self_link
+output "vector_search_index_endpoint_id" {
+  value = module.vector_search.vector_search_index_endpoint_id
 }
 
-output "aicoe_proxy_only_subnet_name" {
-  value = module.network_base.proxy_only_subnet_self_link
+output "vector_search_deployed_index_id" {
+  value = module.vector_search.vector_search_deployed_index_id
 }
 
-output "aicoe_staticip_ilb" {
-  value = module.network_connectivity.reserved_internal_addresses["translation-ilb"].address
-}
-
-output "aicoe_subnet_cidr" {
-  value = module.network_base.subnet_cidr_range
-}
-
-output "psc_endpoint_ip" {
-  value = module.network_connectivity.psc_google_apis_ip
-}
-
-output "dns_zone_name" {
-  value = google_dns_managed_zone.aicoe_googleapis_private.name
-}
-
-output "aicoe_staticip_ilb_salesagent" {
-  value = module.network_connectivity.reserved_internal_addresses["salesagent-ilb"].address
-}
-
-output "aicoe_staticip_ilb_aihub" {
-  value = module.network_connectivity.reserved_internal_addresses["frontend-ilb"].address
-}
-
-output "vector_search_psc_ip_self_link" {
-  value = module.network_connectivity.regional_psc_addresses["vector-index"].self_link
-}
