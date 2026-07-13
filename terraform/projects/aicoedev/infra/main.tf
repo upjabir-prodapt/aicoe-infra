935bef9f74c71c307bb2beb51672518421ac0b67
riyazcloudengineer <riyazcloudengineer@gmail.com>
2026-07-13 21:42:32 +0530

    updated the terraform files in infra and network

--- terraform/projects/aicoedev/infra/main.tf
+++ terraform/projects/aicoedev/infra/main.tf
@@ -1,69 +1,8 @@
-###data "terraform_remote_state" "static" {
-###  backend = "gcs"
-###  config = {
-###    bucket = local.state_bucket
-###    #prefix = "tfstate-static"
-###    prefix = "${var.project_name}/tfstate-static"
-###  }
-###}
-###
-###data "terraform_remote_state" "network" {
-###  backend = "gcs"
-###  config = {
-###    bucket = local.state_bucket
-###    #prefix = "tfstate-network"
-###    prefix = "${var.project_name}/tfstate-static"
-###  }
-###}
-###
-#### NOTE: no module "notebook" block here on purpose. In the old code the
-#### workbench instance was gated by `count = var.envname == "sandox" ? 1 : 0`,
-#### so dev has never actually created one. Adding `module "notebook" {...}`
-#### unconditionally (as aicoesandox does) would show up as "1 to add" in
-#### `terraform plan`. Uncomment/add it later if/when dev is meant to get a
-#### notebook too - see modules/infra-notebook for the interface.
-###
-###module "load_balancer" {
-###  source = "../../../modules/infra-serverless-ilb"
-###
-###  gcp_project_id    = local.gcp_project_id
-###  region            = var.region
-###  resource_prefix   = local.resource_prefix
-###  network_self_link = local.network_self_link
-###  subnet_self_link  = local.subnet_self_link
-###  services          = local.ilb_services
-###  labels            = local.default_labels
-###}
-###
-###module "vector_search" {
-###  source = "../../../modules/infra-vector-search"
-###
-###  gcp_project_id                 = local.gcp_project_id
-###  region                         = var.region
-###  resource_prefix                = local.resource_prefix
-###  network_id                     = local.network_id
-###  vector_search_psc_ip_self_link = data.terraform_remote_state.network.outputs.vector_search_psc_ip_self_link
-###  index_display_name             = "${local.resource_prefix}_salesagent_index"
-###  endpoint_display_name          = "${local.resource_prefix}-salesagent-endpoint"
-###  deployed_index_id              = "${local.resource_prefix}_vector_index"
-###  deployed_index_display_name    = "AICOE salesagent Deployed Index"
-###  labels                         = local.default_labels
-###}
-###
-###module "bigquery" {
-###  source = "../../../modules/infra-bigquery"
-###
-###  gcp_project_id = local.gcp_project_id
-###  region         = var.region
-###  labels         = local.default_labels
-###  datasets       = local.bigquery_datasets
-###}
-### 
-
 data "terraform_remote_state" "static" {
   backend = "gcs"
   config = {
     bucket = local.state_bucket
+    #prefix = "tfstate-static"
     prefix = "${var.project_name}/tfstate-static"
   }
 }
@@ -72,7 +11,8 @@ data "terraform_remote_state" "network" {
   backend = "gcs"
   config = {
     bucket = local.state_bucket
-    prefix = "${var.project_name}/tfstate-network"
+    #prefix = "tfstate-network"
+    prefix = "${var.project_name}/tfstate-static"
   }
 }
 
@@ -118,4 +58,3 @@ module "bigquery" {
   labels         = local.default_labels
   datasets       = local.bigquery_datasets
 }
- 
