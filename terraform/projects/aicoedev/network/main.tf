ec40a088e0585faab5f448fcd0b8902d27e1d0ff
riyazcloudengineer <riyazcloudengineer@gmail.com>
2026-07-13 09:24:55 +0530

    updated the terraform output.tf in infra

--- terraform/projects/aicoedev/network/main.tf
+++ terraform/projects/aicoedev/network/main.tf
@@ -1,6 +1,6 @@
-###########################################
-### Static Layer Remote TF State file   ###
-###########################################
+#############################################
+####  Static Layer Remote TF State file  ####
+#############################################
 
 data "terraform_remote_state" "static" {
   backend = "gcs"
