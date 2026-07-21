5ebe596054463b75cf8e30e77f24221f249b2e86
riyazcloudengineer <riyazcloudengineer@gmail.com>
2026-07-21 13:03:51 +0530

    updated the static-base main and variable files

--- terraform/projects/vegadev/network/locals.tf
+++ terraform/projects/vegadev/network/locals.tf
@@ -1,7 +1,7 @@
 locals {
   gcp_project_id = var.project
 
-  # Matches the original flat resource naming ("${var.project}-vpc" etc.)
+  # Matches the original flat resource naming ("${var.project}-vpc" etc.)#
   resource_prefix = var.project
 
   default_labels = {
