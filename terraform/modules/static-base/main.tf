83af68df1216f3c8bd49869dddf4b4573c0e9b91
BL <LBL@INTERNAL.COLT.NET>
2026-07-22 12:14:05 +0530

    cmo

--- terraform/modules/static-base/main.tf
+++ terraform/modules/static-base/main.tf
@@ -1,48 +1,3 @@
-###resource "google_project_service" "service" {
-###  count              = length(var.gcp_apis_required)
-###  project            = var.gcp_project_id
-###  service            = element(var.gcp_apis_required, count.index)
-###  disable_on_destroy = false
-###
-###  lifecycle {
-###    ignore_changes = [deletion_policy]
-###  }
-###}
-###resource "google_project_iam_audit_config" "data_access" {
-###  for_each = toset(var.audit_services)
-###  project  = var.gcp_project_id
-###  service  = each.value
-###
-###  /*audit_log_config {
-###    log_type = "ADMIN_READ"
-###  }
-###*/
-###  audit_log_config {
-###    log_type = "DATA_READ"
-###  }
-###
-###  audit_log_config {
-###    log_type = "DATA_WRITE"
-###  }
-###}
-###
-###resource "google_tags_tag_key" "env" {
-###  parent     = "projects/${var.gcp_project_id}"
-###  short_name = "environment"
-###}
-###
-###resource "google_tags_tag_value" "env" {
-###  parent     = google_tags_tag_key.env.id
-###  short_name = var.environment_name
-###}
-###
-###resource "google_tags_tag_binding" "env_project" {
-###  parent    = "//cloudresourcemanager.googleapis.com/projects/${var.project_number}"
-###  tag_value = google_tags_tag_value.env.id
-###}
-###
-
-
 resource "google_project_service" "service" {
   count              = length(var.gcp_apis_required)
   project            = var.gcp_project_id
@@ -54,27 +9,6 @@ resource "google_project_service" "service" {
   }
 }
 
-###resource "google_project_iam_audit_config" "data_access" {
-###  for_each = toset([
-###    "storage.googleapis.com",
-###    "aiplatform.googleapis.com",
-###    "bigquery.googleapis.com",
-###  ])
-###  project = var.gcp_project_id
-###  service = each.value
-###
-###  /*audit_log_config {
-###    log_type = "ADMIN_READ"
-###  }
-###*/
-###  audit_log_config {
-###    log_type = "DATA_READ"
-###  }
-###
-###  audit_log_config {
-###    log_type = "DATA_WRITE"
-###  }
-###}
 resource "google_project_iam_audit_config" "data_access" {
   for_each = toset(var.audit_services)
   project  = var.gcp_project_id
--- terraform/modules/static-base/variables.tf
+++ terraform/modules/static-base/variables.tf
@@ -1,25 +1,3 @@
-##variable "gcp_project_id" {
-##  type = string
-##}
-##
-##variable "environment_name" {
-##  type = string
-##}
-##
-##variable "project_number" {
-##  type = string
-##}
-##
-##variable "gcp_apis_required" {
-##  type = list(string)
-##}
-##
-##variable "audit_services" {
-##  type    = list(string)
-##  default = ["storage.googleapis.com", "aiplatform.googleapis.com", "bigquery.googleapis.com"]
-##}
-
-
 variable "gcp_project_id" {
   type = string
 }
