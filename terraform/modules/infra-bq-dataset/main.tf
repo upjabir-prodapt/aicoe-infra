c13b08bcc9f26e4468af657766789886911050c1
BL <LBL@INTERNAL.COLT.NET>
2026-07-22 23:34:53 +0530

    infra

--- terraform/modules/infra-bq-dataset/main.tf
+++
@@ -1,8 +0,0 @@
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id                 = "${var.resource_prefix}_${var.dataset_id_suffix}"
-  location                   = var.region
-  project                    = var.gcp_project_id
-  delete_contents_on_destroy = true
-
-  labels = var.labels
-}
--- terraform/modules/infra-bq-dataset/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dataset_id" {
-  value = google_bigquery_dataset.dataset.dataset_id
-}
-
-output "dataset_self_link" {
-  value = google_bigquery_dataset.dataset.id
-}
