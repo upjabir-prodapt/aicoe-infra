da96f23335eaddeed722c8e697b0db3576434080
BL <LBL@INTERNAL.COLT.NET>
2026-07-22 23:51:43 +0530

    infra

--- terraform/projects/ai-cmo-dev/infra/main.tf
+++ terraform/projects/ai-cmo-dev/infra/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "static" {
   backend = "gcs"
   config = {
     bucket = local.state_bucket
-    prefix = "${var.project_name}/tfstate-static"
+    prefix = "${var.project}/tfstate-static"
   }
 }
 
@@ -10,7 +10,7 @@ data "terraform_remote_state" "network" {
   backend = "gcs"
   config = {
     bucket = local.state_bucket
-    prefix = "${var.project_name}/tfstate-network"
+    prefix = "${var.project}/tfstate-network"
   }
 }
 
