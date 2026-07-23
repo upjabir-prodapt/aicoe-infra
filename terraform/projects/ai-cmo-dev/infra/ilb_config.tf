de5a705f4907a8c121f6cc578cf7129d008096ad
BL <LBL@INTERNAL.COLT.NET>
2026-07-23 13:31:34 +0530

    infra

--- terraform/projects/ai-cmo-dev/infra/ilb_config.tf
+++ terraform/projects/ai-cmo-dev/infra/ilb_config.tf
@@ -1,11 +1,11 @@
-locals {
-  ilb_services = {
+# locals {
+#   ilb_services = {
     
-    salesagent = {
-      cloud_run_service_name = var.cloud_run_service_name
-      ip_address              = local.salesagent_ilb_ip
-      certificate_secret      = "aicoedev-salesagent-ssl-certificate"
-      private_key_secret      = "aicoedev-salesagent-ssl-private-key"
-    }
-  }
-}
+#     salesagent = {
+#       cloud_run_service_name = var.cloud_run_service_name
+#       ip_address              = local.salesagent_ilb_ip
+#       certificate_secret      = "aicoedev-salesagent-ssl-certificate"
+#       private_key_secret      = "aicoedev-salesagent-ssl-private-key"
+#     }
+#   }
+# }
--- terraform/projects/ai-cmo-dev/infra/params.tfvars
+++ terraform/projects/ai-cmo-dev/infra/params.tfvars
@@ -1,4 +1,4 @@
 project                  = "ai-cmo-dev"
 envname                  = "dev"
 region                   = "europe-west1"
-cloud_run_service_name   = "sales-research-application"
+# cloud_run_service_name   = "sales-research-application"
