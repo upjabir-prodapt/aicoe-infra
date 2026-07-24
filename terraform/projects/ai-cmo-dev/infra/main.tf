bfa5b803e8fb5d22079e526f511b8298a141a6d6
BL <LBL@INTERNAL.COLT.NET>
2026-07-24 16:15:59 +0530

    infra

--- terraform/projects/ai-cmo-dev/infra/main.tf
+++ terraform/projects/ai-cmo-dev/infra/main.tf
@@ -28,20 +28,20 @@ module "load_balancer" {
   labels            = local.default_labels
 }
 
-# module "vector_search" {
-#   source = "../../../modules/infra-vector-search"
+module "vector_search" {
+  source = "../../../modules/infra-vector-search"
 
-#   gcp_project_id                 = local.gcp_project_id
-#   region                         = var.region
-#   resource_prefix                = local.resource_prefix
-#   network_id                     = local.network_id
-#   vector_search_psc_ip_self_link = data.terraform_remote_state.network.outputs.vector_search_psc_ip_self_link
-#   index_display_name             = "${local.resource_prefix}_salesagent_index"
-#   endpoint_display_name          = "${local.resource_prefix}-salesagent-endpoint"
-#   deployed_index_id              = "${local.resource_prefix}_vector_index"
-#   deployed_index_display_name    = "AICOE salesagent Deployed Index"
-#   labels                         = local.default_labels
-# }
+  gcp_project_id                 = local.gcp_project_id
+  region                         = var.region
+  resource_prefix                = local.resource_prefix
+  network_id                     = local.network_id
+  vector_search_psc_ip_self_link = data.terraform_remote_state.network.outputs.vector_search_psc_ip_self_link
+  index_display_name             = "${local.resource_prefix}_salesagent_index"
+  endpoint_display_name          = "${local.resource_prefix}-salesagent-endpoint"
+  deployed_index_id              = "${local.resource_prefix}_vector_index"
+  deployed_index_display_name    = "Salesagent Deployed Index"
+  labels                         = local.default_labels
+}
 
 module "bigquery" {
   source = "../../../modules/infra-bigquery"
