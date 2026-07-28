c26d6e1610b2ee68d20b437fcaafc86c38c15792
BL <LBL@INTERNAL.COLT.NET>
2026-07-28 13:46:49 +0530

    dev

--- terraform/projects/aicoedev/static/import.tf
+++ terraform/projects/aicoedev/static/import.tf
@@ -1,19 +1,29 @@
+# import {
+#     to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/compute.osAdminLogin"]
+#     id = "aicoedev roles/compute.osAdminLogin group:Aicoedev-platformadmin@colt.net"
+# }
+
+# import {
+#     to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/iap.httpsResourceAccessor"]
+#     id = "aicoedev roles/iap.httpsResourceAccessor group:Aicoedev-platformadmin@colt.net"
+# }
+
+# import {
+#     to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/owner"]
+#     id = "aicoedev roles/owner group:Aicoedev-platformadmin@colt.net"
+# }
+
+# import {
+#     to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/iap.admin"]
+#     id = "aicoedev roles/iap.admin group:Aicoedev-platformadmin@colt.net"
+# }
+
 import {
-    to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/compute.osAdminLogin"]
-    id = "aicoedev roles/compute.osAdminLogin group:Aicoedev-platformadmin@colt.net"
+    to = module.identities.google_service_account.sa["ui"]
+    id = "projects/aicoedev/serviceAccounts/aicoedev-ui-sa@aicoedev.iam.gserviceaccount.com"
 }
 
 import {
-    to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/iap.httpsResourceAccessor"]
-    id = "aicoedev roles/iap.httpsResourceAccessor group:Aicoedev-platformadmin@colt.net"
-}
-
-import {
-    to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/owner"]
-    id = "aicoedev roles/owner group:Aicoedev-platformadmin@colt.net"
-}
-
-import {
-    to = module.group_iam["Aicoedev-platformadmin@colt.net"].google_project_iam_member.group_members["roles/iap.admin"]
-    id = "aicoedev roles/iap.admin group:Aicoedev-platformadmin@colt.net"
+    to = module.identities.google_service_account.sa["app"]
+    id = "projects/aicoedev/serviceAccounts/aicoedev-app-sa@aicoedev.iam.gserviceaccount.com"
 }
